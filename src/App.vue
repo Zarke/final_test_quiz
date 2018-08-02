84897e12e92edb875c97ed3b8b333b76e310a079
Zarke <milanzaric54@gmail.com>
2018-08-02 14:14:09 +0200

    minor modifications to the results table rendering

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,27 @@
     <div class="container">
         <template v-if="quizStart">
             <template v-if="quizEnd" >
-                <span >Congratulations {{results['username']}}, you finshed the quiz in {{results['totalTime']}} seconds with {{results['totalPoints']}} points</span>
-                <app-restart @quizFinished="quizEnd = false; quizStart=false"></app-restart>
+                <span >Congratulations {{result['username']}}, you finshed the quiz in {{result['totalTime']}} seconds with {{result['totalPoints']}} points</span>
+                <app-restart @quizFinished="quizEnd = false; quizStart=false; "></app-restart>
             </template>
             <template v-else>
                 <app-timer :quizFinished="quizEnd"
-                            @totalTime="updateResults($event)">
+                            @totalTime="updateResult($event)">
                 </app-timer>
                 <app-questions :questions="questionsArr">
                 </app-questions>
-                <app-points  @totalPoints="updateResults($event)"></app-points>
+                <app-points  @totalPoints="updateResult($event)"></app-points>
             </template>
         </template>
         <template v-else>
-            <app-start-form @quizStarted="quizStart = true"
-                            @username="updateResults($event)"
-            >
-            </app-start-form>
             <div id="table" class="col-xs-12 table-responsive">
                 <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
             </div>
+            <app-start-form @quizStarted="quizStart = true"
+                            @username="updateResult($event)"
+            >
+            </app-start-form>
+            
         </template>      
     </div>
 </template>
@@ -41,32 +42,30 @@
                 quizStart: false,
                 quizEnd: false,
                 questionsArr: Array,
-                results: {'username':'', 'totalTime':'', 'totalPoints':''},//results of the current quiz run  
+                result: {'username':'', 'totalTime':'', 'totalPoints':''},//results of the current quiz run  
                 columns: ['name', 'userPoints', 'date', 'time'],
                 tableData: [],
                 options: {
-                    filterByColumn: true,
                     headings: {
                         name: 'Name',
                         userPoints: 'Points',
                         date: 'Date',
                         time: 'Time',
-                      },
-                },
-                sortOrder: [
-                {
-                  field: 'userPoints',
-                  direction: 'asc'
+                    },
+                    orderBy:{ascending:false,'column': 'userPoints'},
+                    clientMultiSorting:true 
                 }
-    ]
             }
         },
         methods:{
-            updateResults(resultsParams){
-                if( resultsParams[1] === '' && resultsParams[0] == 'username' ){
-                    this.result[resultsParams[0]] = 'anonymous';
+            updateResult(resultParams){
+                if( resultParams[1] === '' && resultParams[0] == 'username' ){
+                    this.result[resultParams[0]] = 'anonymous';
                 }
-                this.results[resultsParams[0]] = resultsParams[1];
+                this.result[resultParams[0]] = resultParams[1];
+            },
+            updateResultsJSON(entry){
+                
             }
         },
         created(){
